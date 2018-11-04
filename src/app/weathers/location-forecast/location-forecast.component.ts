a67af8f50347e0870f42a238ad242569c3dd27cd
thomascsd <thomascsd808@gmail.com>
2018-11-05 07:45:35 +0800

    加上根籒城市的查詢的功能

--- src/app/weathers/location-forecast/location-forecast.component.ts
+++ src/app/weathers/location-forecast/location-forecast.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LocationForecastQuery } from '../state/location-forecast.query';
+import { ForecastService } from '../state/forecast.service';
+import { ForecastDatum } from '../state';
 
 @Component({
   selector: 'app-location-forecast',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location-forecast.component.scss']
 })
 export class LocationForecastComponent implements OnInit {
+  forecastData$: Observable<ForecastDatum[]>;
 
-  constructor() { }
+  constructor(
+    private query: LocationForecastQuery,
+    private servie: ForecastService
+  ) {}
 
   ngOnInit() {
+    this.loadData('tokyo');
   }
 
+  changeCity(city: string) {
+    this.loadData(city);
+  }
+
+  private loadData(city: string) {
+    this.forecastData$ = this.query.selectAll({
+      limitTo: 5
+    });
+    this.servie.getLocation(city);
+  }
 }
