a949a6f5033424cc2e689017e1a553d7f2a180f5
Thomas Chang <thomascsd808@gmail.com>
2021-08-20 07:17:01 +0800

    fix: postion not found

--- src/app/weathers/days-forecast/days-forecast.component.ts
+++ src/app/weathers/days-forecast/days-forecast.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  DaysForecastQuery,
-  ForecastService,
-  ForecastDatum,
-  DaysForecastStore
-} from '../state';
+import { DaysForecastQuery, ForecastService, ForecastDatum, DaysForecastStore } from '../state';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-days-forecast',
   templateUrl: './days-forecast.component.html',
-  styleUrls: ['./days-forecast.component.scss']
+  styleUrls: ['./days-forecast.component.scss'],
 })
 export class DaysForecastComponent implements OnInit {
   forecastData$: Observable<ForecastDatum[]>;
@@ -24,15 +19,15 @@ export class DaysForecastComponent implements OnInit {
 
   ngOnInit() {
     navigator.geolocation.getCurrentPosition(
-      (pos: Position) => {
+      (pos: GeolocationPosition) => {
         this.store.setLoading(true);
         this.forecastData$ = this.query.selectAll({
-          limitTo: 8
+          limitTo: 8,
         });
         this.loading$ = this.query.selectLoading();
         this.service.getDays(pos.coords.latitude, pos.coords.longitude);
       },
-      error => {
+      (error) => {
         console.log(error);
       }
     );
